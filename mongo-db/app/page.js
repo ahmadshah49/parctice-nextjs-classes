37aa48772f05054157102d73784cc146395ab4c9
ahmadshah49 <ahmadraza.fsd.pk94@gmail.com>
2023-11-24 21:19:57 +0500

    mongo db crud

--- mongo-db/app/page.js
+++ mongo-db/app/page.js
@@ -1,18 +1,31 @@
-import dbConnect from '@/config/db'
-import { userModel } from '@/model/users'
-import Image from 'next/image'
-dbConnect()
-const fetchData =async ()=>{
-  try {
-    const data=await userModel.find()
-    console.log("data",data);
-    return data
-  } catch (error) {
-    console.log("error",error);
+"use client"
+import { useEffect,useState } from "react"
+// import dbConnect from '@/config/db'
+// import { userModel } from '@/model/users'
+// import Image from 'next/image'
+// dbConnect()
+// const fetchData =async ()=>{
+//   try {
+//     const data=await userModel.find()
+//     console.log("data",data);
+//     return data
+//   } catch (error) {
+//     console.log("error",error);
+//   }
+// }
+   function Home() {
+  // const data=await fetchData()
+  const [data, setData] = useState([])
+ const  fetchUsers=async ()=>{
+    const response= await fetch("http://localhost:3000/api/profiles")
+    const result=await response.json()
+    console.log("results",result);
+    setData(result.profiles)
   }
-}
- async function Home() {
-  const data=await fetchData()
+useEffect(() => {
+ fetchUsers()
+}, [])
+
   return (
   <>
   {
